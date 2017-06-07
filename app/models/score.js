77f7d120cc80f733fd8f32b864008839fa7e9e6b
jim-moody <jim.moody@libertymutual.com>
2017-06-07 11:21:56 -0400

    Fix the editable field
    Because we are now doing a populate of the owner object, we have to go one level deeper to get the id

--- app/models/score.js
+++ app/models/score.js
@@ -26,7 +26,7 @@ const scoreSchema = new mongoose.Schema({
     virtuals: true,
     transform: function (doc, ret, options) {
       const userId = (options.user && options.user._id) || false
-      ret.editable = userId && userId.equals(doc._owner)
+      ret.editable = userId && userId.equals(doc._owner._id)
       return ret
     }
   }
